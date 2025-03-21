adf989e9bd0f6203ebb1fa3665a9235f617b8041
CinquinAndy <cinquin.andy@gmail.com>
2025-03-21 17:16:59 +0100

    feat(marketing): revamp footer and pricing sections
    - Replace footer layout with a new design featuring a call-to-action
    - Introduce sitemap for better navigation
    - Update social media links and icons
    - Remove old pricing section, integrate new PricingCards component
    - Enhance hero section with improved text formatting and content clarity

--- src/app/(marketing)/components/footer.tsx
+++ src/app/(marketing)/components/footer.tsx
@@ -1,11 +1,97 @@
-import { Container } from './container'
-import { Link } from './link'
+import {
+	PlusGrid,
+	PlusGridItem,
+	PlusGridRow,
+} from '@/app/(marketing)/components/plus-grid'
 
-function SocialIconX(props: React.ComponentPropsWithoutRef<'svg'>) {
+import { Button } from './button'
+import { Container } from './container'
+import { Gradient } from './gradient'
+import { Link } from './link'
+import { Logo } from './logo'
+import { Subheading } from './text'
+
+function CallToAction() {
 	return (
-		<svg viewBox='0 0 16 16' fill='currentColor' {...props}>
-			<path d='M12.6 0h2.454l-5.36 6.778L16 16h-4.937l-3.867-5.594L2.771 16H.316l5.733-7.25L0 0h5.063l3.495 5.114L12.6 0zm-.86 14.376h1.36L4.323 1.539H2.865l8.875 12.837z' />
-		</svg>
+		<div className='relative pt-20 pb-16 text-center sm:py-24'>
+			<hgroup>
+				<Subheading>Commencer</Subheading>
+				<p className='mt-6 text-3xl font-medium tracking-tight text-gray-950 sm:text-5xl'>
+					{`Prêt à optimiser votre gestion?`}
+					<br />
+					Essayez gratuitement.
+				</p>
+			</hgroup>
+
+			<p className='mx-auto mt-6 max-w-xs text-sm/6 text-gray-500'>
+				{`Simplifiez le suivi de vos équipements et optimisez l'utilisation de votre parc matériel.`}
+			</p>
+			<div className='mt-6'>
+				<Button className='w-full sm:w-auto' href='#'>
+					{`Démarrer l'essai`}
+				</Button>
+			</div>
+		</div>
+	)
+}
+
+function SitemapHeading({ children }: { children: React.ReactNode }) {
+	return <h3 className='text-sm/6 font-medium text-gray-950/50'>{children}</h3>
+}
+
+function SitemapLinks({ children }: { children: React.ReactNode }) {
+	return <ul className='mt-6 space-y-4 text-sm/6'>{children}</ul>
+}
+
+function SitemapLink(props: React.ComponentPropsWithoutRef<typeof Link>) {
+	return (
+		<li>
+			<Link
+				{...props}
+				className='font-medium text-gray-950 data-hover:text-gray-950/75'
+			/>
+		</li>
+	)
+}
+
+function Sitemap() {
+	return (
+		<>
+			<div>
+				<SitemapHeading>Produit</SitemapHeading>
+				<SitemapLinks>
+					<SitemapLink href='/pricing'>Tarification</SitemapLink>
+					{/* <SitemapLink href='#'>Fonctionnalités</SitemapLink> */}
+					{/* <SitemapLink href='#'>API</SitemapLink> */}
+				</SitemapLinks>
+			</div>
+			<div>
+				<SitemapHeading>Entreprise</SitemapHeading>
+				<SitemapLinks>
+					{/* <SitemapLink href='#'>Carrières</SitemapLink> */}
+					<SitemapLink href='/blog'>Blog</SitemapLink>
+					<SitemapLink href='/company'>{`À propos`}</SitemapLink>
+				</SitemapLinks>
+			</div>
+			<div>
+				<SitemapHeading>Support</SitemapHeading>
+				<SitemapLinks>
+					{/* <SitemapLink href='#'>Centre d'aide</SitemapLink> */}
+					<SitemapLink href='mailto:contact@fortooling.fr'>
+						Contactez-nous
+					</SitemapLink>
+				</SitemapLinks>
+			</div>
+			<div>
+				<SitemapHeading>Légal</SitemapHeading>
+				<SitemapLinks>
+					<SitemapLink href='/cgu'>{`Conditions d'utilisation`}</SitemapLink>
+					<SitemapLink href='/privacy'>
+						{`Politique de confidentialité`}
+					</SitemapLink>
+				</SitemapLinks>
+			</div>
+		</>
 	)
 }
 
@@ -29,29 +115,40 @@ function SocialIconLinkedIn(props: React.ComponentPropsWithoutRef<'svg'>) {
 	)
 }
 
+function SocialIconInstagram(props: React.ComponentPropsWithoutRef<'svg'>) {
+	return (
+		<svg viewBox='0 0 24 24' fill='currentColor' {...props}>
+			<path
+				xmlns='http://www.w3.org/2000/svg'
+				d='M7.0301.084c-1.2768.0602-2.1487.264-2.911.5634-.7888.3075-1.4575.72-2.1228 1.3877-.6652.6677-1.075 1.3368-1.3802 2.127-.2954.7638-.4956 1.6365-.552 2.914-.0564 1.2775-.0689 1.6882-.0626 4.947.0062 3.2586.0206 3.6671.0825 4.9473.061 1.2765.264 2.1482.5635 2.9107.308.7889.72 1.4573 1.388 2.1228.6679.6655 1.3365 1.0743 2.1285 1.38.7632.295 1.6361.4961 2.9134.552 1.2773.056 1.6884.069 4.9462.0627 3.2578-.0062 3.668-.0207 4.9478-.0814 1.28-.0607 2.147-.2652 2.9098-.5633.7889-.3086 1.4578-.72 2.1228-1.3881.665-.6682 1.0745-1.3378 1.3795-2.1284.2957-.7632.4966-1.636.552-2.9124.056-1.2809.0692-1.6898.063-4.948-.0063-3.2583-.021-3.6668-.0817-4.9465-.0607-1.2797-.264-2.1487-.5633-2.9117-.3084-.7889-.72-1.4568-1.3876-2.1228C21.2982 1.33 20.628.9208 19.8378.6165 19.074.321 18.2017.1197 16.9244.0645 15.6471.0093 15.236-.005 11.977.0014 8.718.0076 8.31.0215 7.0301.0839m.1402 21.6932c-1.17-.0509-1.8053-.2453-2.2287-.408-.5606-.216-.96-.4771-1.3819-.895-.422-.4178-.6811-.8186-.9-1.378-.1644-.4234-.3624-1.058-.4171-2.228-.0595-1.2645-.072-1.6442-.079-4.848-.007-3.2037.0053-3.583.0607-4.848.05-1.169.2456-1.805.408-2.2282.216-.5613.4762-.96.895-1.3816.4188-.4217.8184-.6814 1.3783-.9003.423-.1651 1.0575-.3614 2.227-.4171 1.2655-.06 1.6447-.072 4.848-.079 3.2033-.007 3.5835.005 4.8495.0608 1.169.0508 1.8053.2445 2.228.408.5608.216.96.4754 1.3816.895.4217.4194.6816.8176.9005 1.3787.1653.4217.3617 1.056.4169 2.2263.0602 1.2655.0739 1.645.0796 4.848.0058 3.203-.0055 3.5834-.061 4.848-.051 1.17-.245 1.8055-.408 2.2294-.216.5604-.4763.96-.8954 1.3814-.419.4215-.8181.6811-1.3783.9-.4224.1649-1.0577.3617-2.2262.4174-1.2656.0595-1.6448.072-4.8493.079-3.2045.007-3.5825-.006-4.848-.0608M16.953 5.5864A1.44 1.44 0 1 0 18.39 4.144a1.44 1.44 0 0 0-1.437 1.4424M5.8385 12.012c.0067 3.4032 2.7706 6.1557 6.173 6.1493 3.4026-.0065 6.157-2.7701 6.1506-6.1733-.0065-3.4032-2.771-6.1565-6.174-6.1498-3.403.0067-6.156 2.771-6.1496 6.1738M8 12.0077a4 4 0 1 1 4.008 3.9921A3.9996 3.9996 0 0 1 8 12.0077'
+			/>
+		</svg>
+	)
+}
+
 function SocialLinks() {
 	return (
 		<>
 			<Link
 				href='https://facebook.com'
 				target='_blank'
-				aria-label='Visit us on Facebook'
+				aria-label='Visitez notre Facebook'
 				className='text-gray-950 data-hover:text-gray-950/75'
 			>
 				<SocialIconFacebook className='size-4' />
 			</Link>
 			<Link
-				href='https://x.com'
+				href='https://facebook.com'
 				target='_blank'
-				aria-label='Visit us on X'
+				aria-label='Visitez notre Facebook'
 				className='text-gray-950 data-hover:text-gray-950/75'
 			>
-				<SocialIconX className='size-4' />
+				<SocialIconInstagram className='size-4' />
 			</Link>
 			<Link
 				href='https://linkedin.com'
 				target='_blank'
-				aria-label='Visit us on LinkedIn'
+				aria-label='Visitez notre LinkedIn'
 				className='text-gray-950 data-hover:text-gray-950/75'
 			>
 				<SocialIconLinkedIn className='size-4' />
@@ -63,90 +160,46 @@ function SocialLinks() {
 function Copyright() {
 	return (
 		<div className='text-sm/6 text-gray-950'>
-			&copy; {new Date().getFullYear()} ForTooling. Tous droits réservés.
+			&copy; {new Date().getFullYear()} ForTooling. {`Tous droits réservés.`}
 		</div>
 	)
 }
 
 export function Footer() {
 	return (
-		<footer className='mt-24 sm:mt-32 lg:mt-40'>
-			<Container>
-				<div className='grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-2'>
-					<div className='flex flex-col'>
-						<div className='flex h-12 w-auto items-center text-xl font-bold'>
-							ForTooling
-						</div>
-						<p className='mt-6 text-sm text-gray-600'>
-							{`ForTooling simplifie la gestion de votre parc d'équipements grâce
-							à la technologie NFC/QR. Localisez, attribuez et entretenez vos
-							équipements en toute simplicité.`}
-						</p>
-						<div className='mt-6 flex gap-x-4'>
-							<SocialLinks />
-						</div>
-					</div>
-
-					<div className='grid grid-cols-2 gap-8 sm:grid-cols-3'>
-						<div>
-							<h3 className='text-sm font-semibold'>Produit</h3>
-							<ul className='mt-6 space-y-4 text-sm'>
-								<li>
-									<Link
-										href='/features'
-										className='text-gray-600 hover:text-gray-950'
-									>
-										{'Fonctionnalités'}
-									</Link>
-								</li>
-							</ul>
-						</div>
-
-						<div>
-							<h3 className='text-sm font-semibold'>Company</h3>
-							<ul className='mt-6 space-y-4 text-sm'>
-								<li>
-									<Link href='#'>Careers</Link>
-								</li>
-								<li>
-									<Link href='/blog'>Blog</Link>
-								</li>
-								<li>
-									<Link href='/company'>Company</Link>
-								</li>
-							</ul>
-						</div>
-
-						<div>
-							<h3 className='text-sm font-semibold'>Support</h3>
-							<ul className='mt-6 space-y-4 text-sm'>
-								<li>
-									<Link href='#'>Help center</Link>
-								</li>
-								<li>
-									<Link href='#'>Community</Link>
-								</li>
-							</ul>
-						</div>
-
-						<div>
-							<h3 className='text-sm font-semibold'>Company</h3>
-							<ul className='mt-6 space-y-4 text-sm'>
-								<li>
-									<Link href='#'>Terms of service</Link>
-								</li>
-								<li>
-									<Link href='#'>Privacy policy</Link>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-
-				<div className='mt-16 border-t border-gray-900/5 pt-8 sm:mt-20 sm:pt-10 md:mt-24 lg:mt-32'>
-					<Copyright />
-				</div>
-			</Container>
+		<footer>
+			<Gradient className='relative'>
+				<div className='absolute inset-2 rounded-4xl bg-white/80' />
+				<Container>
+					<CallToAction />
+					<PlusGrid className='pb-16'>
+						<PlusGridRow>
+							<div className='grid grid-cols-2 gap-y-10 pb-6 lg:grid-cols-6 lg:gap-8'>
+								<div className='col-span-2 flex'>
+									<PlusGridItem className='pt-6 lg:pb-6'>
+										<Logo className='h-9 w-9' />
+									</PlusGridItem>
+								</div>
+								<div className='col-span-2 grid grid-cols-2 gap-x-8 gap-y-12 lg:col-span-4 lg:grid-cols-subgrid lg:pt-6'>
+									<Sitemap />
+								</div>
+							</div>
+						</PlusGridRow>
+						<PlusGridRow className='flex justify-between'>
+							<div>
+								<PlusGridItem className='py-3'>
+									<Copyright />
+								</PlusGridItem>
+							</div>
+							<div className='flex'>
+								<PlusGridItem className='flex items-center gap-8 py-3'>
+									<SocialLinks />
+								</PlusGridItem>
+							</div>
+						</PlusGridRow>
+					</PlusGrid>
+				</Container>
+			</Gradient>
 		</footer>
 	)
 }
