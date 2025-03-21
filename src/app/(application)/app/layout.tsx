3c12e649f5af812da04650a79f163ed19fb5de35
CinquinAndy <cinquin.andy@gmail.com>
2025-03-21 13:37:54 +0100

    feat(radiant-theme): install and configure the radiant theme

---
+++ src/app/(application)/app/layout.tsx
@@ -0,0 +1,21 @@
+import type React from 'react'
+
+import { AppSidebar } from '@/components/app/app-sidebar'
+import { TopBar } from '@/components/app/top-bar'
+import { SidebarProvider } from '@/components/ui/sidebar'
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<div className={`h-full min-h-screen`}>
+			<SidebarProvider defaultOpen={true}>
+				<AppSidebar />
+				<div className='ml-20 min-h-screen w-full bg-[#0f2942]'>
+					<TopBar />
+					<main className='min-h-[calc(100vh-4rem)] w-full overflow-auto rounded-tl-xl bg-stone-50'>
+						<div className='h-full w-full max-w-full p-4'>{children}</div>
+					</main>
+				</div>
+			</SidebarProvider>
+		</div>
+	)
+}
